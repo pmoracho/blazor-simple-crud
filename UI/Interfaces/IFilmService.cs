3a35d343ff18ae7f2da68019e02dcc6e33bf7261
Patricio Moracho <pmoracho@gmail.com>
2023-09-26 21:18:48 -0300

    Incorporo radzen e implemento la grilla un poco mejor

--- UI/Interfaces/IFilmService.cs
+++ UI/Interfaces/IFilmService.cs
@@ -4,7 +4,7 @@ namespace UI.Interfaces {
 
     public interface IFilmService {
 
-        Task<IEnumerable<Film>>? GetAllFilms();
+        Task<IEnumerable<Film>> GetAllFilms();
         Task<Film> GetFilmDetails(int id);
         Task<bool> SaveFilm(Film film);
         Task<bool> DeleteFilm(int id);
